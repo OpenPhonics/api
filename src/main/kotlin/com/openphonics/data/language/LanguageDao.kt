f911903683779d403dd6f3170e02406cdbd6ce7a
Advait Vedant <advait.suraj.vedant@gmail.com>
2023-11-05 21:20:58 -0800

    added language and word dao

--- src/main/kotlin/com/openphonics/data/language/LanguageDao.kt
+++ src/main/kotlin/com/openphonics/data/language/LanguageDao.kt
@@ -1,9 +1,56 @@
 package com.openphonics.data.language
 
+import com.openphonics.data.flag.FlagEntity
+import com.openphonics.utils.OpenPhonicsMapper
+import org.jetbrains.exposed.sql.transactions.transaction
+import javax.inject.Inject
+import javax.inject.Singleton
+
 interface LanguageDao {
     fun create(nativeId: String, languageId: String, languageName: String, flag: String): Int
     fun all(nativeId: String): List<Language>
     fun update(id: Int, nativeId: String? = null, languageId: String? = null, languageName: String? = null, flag: String? = null): Int
     fun get(id: Int): Language?
     fun delete(id: Int): Boolean
+}
+@Singleton
+class LanguageDaoImpl @Inject constructor(
+    private val mapper: OpenPhonicsMapper
+) : LanguageDao {
+    override fun create(nativeId: String, languageId: String, languageName: String, flag: String): Int = transaction {
+        LanguageEntity.new {
+            this.nativeId = nativeId
+            this.languageId = languageId
+            this.languageName = languageName
+            this.flag = FlagEntity[flag]
+        }.id.value
+    }
+
+    override fun all(nativeId: String): List<Language> = transaction {
+        LanguageEntity.find {
+            (Languages.nativeId eq nativeId)
+        }.map { mapper.fromEntity(it) }
+    }
+
+    override fun update(id: Int, nativeId: String?, languageId: String?, languageName: String?, flag: String?): Int =
+        transaction {
+            LanguageEntity[id].apply {
+                nativeId?.let { this.nativeId = it }
+                languageId?.let { this.languageId = it }
+                languageName?.let { this.languageName = it }
+                flag?.let { this.flag = FlagEntity[flag] }
+            }.id.value
+        }
+
+    override fun get(id: Int): Language? = transaction {
+        LanguageEntity.findById(id)
+    }?.let { mapper.fromEntity(it) }
+
+    override fun delete(id: Int): Boolean = transaction {
+        LanguageEntity.findById(id)?.run {
+            delete()
+            return@transaction true
+        }
+        return@transaction false
+    }
 }
--- src/main/kotlin/com/openphonics/data/word/WordDao.kt
+++ src/main/kotlin/com/openphonics/data/word/WordDao.kt
@@ -1,5 +1,12 @@
 package com.openphonics.data.word
 
+import com.openphonics.data.language.LanguageEntity
+import com.openphonics.utils.OpenPhonicsMapper
+import org.jetbrains.exposed.sql.and
+import org.jetbrains.exposed.sql.transactions.transaction
+import javax.inject.Inject
+import javax.inject.Singleton
+
 interface WordDao {
     fun create(language: Int, phonic: String, sound: String, translatedWord: String, translatedSound: String, word: String): Int
     fun update(
@@ -14,4 +21,67 @@ interface WordDao {
     fun get(language: Int, word: String): Word?
     fun all(language: Int): List<Word>
     fun delete(id: Int): Boolean
+}
+@Singleton
+class WordDaoImpl @Inject constructor(
+    private val mapper: OpenPhonicsMapper
+) : WordDao {
+    override fun create(
+        language: Int,
+        phonic: String,
+        sound: String,
+        translatedWord: String,
+        translatedSound: String,
+        word: String
+    ): Int = transaction {
+        WordEntity.new {
+            this.phonic = phonic
+            this.sound = sound
+            this.translatedWord = translatedWord
+            this.translatedSound = translatedSound
+            this.word = word
+            this.language = LanguageEntity[language]
+        }.id.value
+    }
+
+    override fun update(
+        id: Int,
+        phonic: String?,
+        sound: String?,
+        translatedWord: String?,
+        translatedSound: String?,
+        word: String?
+    ): Int = transaction {
+        WordEntity[id].apply {
+            phonic?.let { this.phonic = it }
+            sound?.let { this.sound = it }
+            translatedWord?.let { this.translatedWord = it }
+            translatedSound?.let { this.translatedSound = it }
+            word?.let { this.word = it }
+        }.id.value
+    }
+
+    override fun get(id: Int): Word? = transaction {
+        WordEntity.findById(id)
+    }?.let { mapper.fromEntity(it) }
+
+    override fun get(language: Int, word: String): Word? = transaction {
+        WordEntity.find {
+            ((Words.language eq language) and (Words.word eq word))
+        }.firstOrNull()
+    }?.let { mapper.fromEntity(it) }
+
+    override fun all(language: Int): List<Word> = transaction {
+        WordEntity.find {
+            (Words.language eq language)
+        }.map { mapper.fromEntity(it) }
+    }
+
+    override fun delete(id: Int): Boolean = transaction {
+        WordEntity.findById(id)?.run {
+            delete()
+            return@transaction true
+        }
+        return@transaction false
+    }
 }
